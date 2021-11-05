b5fe1be6f9b202c4f6b0e47681f31035085ab052
harsh gautam <androidmasterid>
2021-11-05 10:44:25 +0530

    latest ui

---
+++ script/script.js
@@ -0,0 +1,40 @@
+const sr = ScrollReveal();
+
+sr.reveal('.popUpBox', {
+    duration: 1000,
+    delay: 1000,
+})
+
+sr.reveal('.opt1', {
+    origin: 'top',
+    duration: 1000,
+    delay: 2000,
+})
+sr.reveal('.opt2', {
+
+
+    origin: 'top',
+    duration: 1000,
+    delay: 4000,
+})
+sr.reveal('.opt3', {
+
+    // distance: '30%',
+    origin: 'top',
+    duration: 1000,
+    delay: 6000,
+})
+sr.reveal('.opt4', {
+
+    // distance: '30%',
+    origin: 'top',
+    duration: 1000,
+    delay: 8000,
+})
+sr.reveal('.btnn', {
+
+    // distance: '30%',
+    origin: 'top',
+    duration: 1000,
+    delay: 10000,
+})
